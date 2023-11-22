4a005cdf4ea8f73169e591a1ea152ada041bae5c
Katia1306 <kateryna.zhylan@gmail.com>
2023-11-22 20:07:57 +0100

    kk

--- practice.js
+++ practice.js
@@ -249,13 +249,436 @@ console.log(updateCourse('Express', 'NestJS'));
 // console.log(values);
 
 //*********************** PRACTICE **********/
-function createBasket(product, quantity, price) {
-    const basket = {
-        name: product,
-        quantity,
-        price,
-        totalPrice: quantity * price,
-    };
-    return basket
-}
-console.log(basket);
+
+//function createBasket(product, quantity, price) {
+    // const basket = {
+    //     name: product,
+    //     quantity,
+    //     price,
+    //     totalPrice: quantity * price,
+    // };
+    // return basket
+
+//     return {
+//        name: product,
+//        quantity,
+//        price,
+//        totalPrice: quantity * price, 
+//     }
+// }
+// console.log(createBasket("pizza", 3, 120));
+
+//************************************************** */
+ 
+// const players = {
+//     Den: 60,
+//     Kate: 130,
+//     William: 45,
+//     Matthew: 120,
+//     Ethan: 40,
+//     David: 55,
+// };
+
+// function getTime(obj) {
+//     const countPlayers = Object.keys(obj)?.length;
+//     const values = Object.values(obj);
+//     //console.log(values);
+//     let totalTime = 0;
+
+//     for (const value of values) {
+//         totalTime += value;
+//     }
+//     //console.log(totalTime);
+//     return `Count of players ${countPlayers}, average time ${totalTime / countPlayers}`
+// }
+// console.log(getTime(players));
+
+//********************************************* */
+// const friends = [
+//     { name: "Anna", books: ["Bible", "Harry Potter"], age: 21 },
+//     { name: "Bob", books: ["War and Peace", "Romeo and Juliet"], age: 26 },
+//     { name: "Alice", books: ["War and Peace", "Romeo and Juliet"] },
+//     { name: "Oleksii", books: ["Bible", "War and Peace", "Harry Potter", "Romeo and Juliet"], age: 26}
+// ]
+
+// function getUsers(arr, bookName) {
+//     const users = []
+//     for (const user of arr) {
+//         if (user.books.includes(bookName)) {
+//             //console.log(user.name);
+//             users.push(user.name)
+//        }
+//     } 
+//     //console.log(users);
+//     return users.join(', ')
+// }
+// console.log(friends, "Harry Potter");
+
+// function getTotalAge(arr) {
+//     let totalAge = 0;
+//     for (const user of arr) {
+//         if (user.hasOwnProperty('age')) {
+//             totalAge += user.age;
+//         }
+//     }
+//     return totalAge;
+// }
+// console.log (getTotalAge(friends));
+
+//******************************************* */
+
+// const hogvards = {
+//     griffindor: [{
+//         name: "Harry",
+//         points: 17
+//     },
+//     {
+//         name: "Hermiona",
+//         points: 19
+//     },
+//     {
+//         name: "Ron",
+//         points: 14
+//     }],
+//     slizerin: [{
+//         name: "Draco",
+//         points: 17
+//     },
+//     {
+//         name: "Goyl",
+//         points: 14
+//     },
+//     {
+//         name: "Grabbe",
+//         points: 5
+//         }],
+    
+//     getTotalPoints(facultyName) {
+//         if (!this.hasOwnProperty(facultyName)) {
+//             return "Not found";
+//         }
+
+//         let totalPoints = 0;
+
+//         for (const student of this[facultyName]) {
+            
+//             totalPoints += student.points
+            
+//         }
+
+//         return totalPoints
+//     },
+//     };
+
+//  console.log(hogvards.getTotalPoints("griffindor"));
+//  console.log(hogvards.getTotalPoints("slizerin"));
+    
+  
+    
+    
+    
+//     getUserList(facultyName) {
+//         if (!(facultyName in this)) {
+//             return "Not found";
+//         }
+//         const students = [];
+
+//         for (const student of this[facultyName]) {
+//             students.push(student.name);
+//         }
+//         return students.join(', ')
+//     },
+// };
+
+// console.log(hogvards.getUserList("griffindor"));
+// console.log(hogvards.getUserList("slizerin")); //
+
+
+//********----Distructurization------****/
+// const user = {
+//     name: 'Test name',
+//     skills: {
+//         html: true,
+//         css: false,
+//         js: true
+//     }
+// };
+// function getUserName({ name, skills: { html, css, js } }) {
+//     console.log(
+//         `Hello my name is ${name}, I know html - ${html}, css - ${css} and js - ${js}`
+//     );
+// }
+// getUserName(user);
+
+//*****************************++++++*** */
+//const users = [{ name: 'Kate' }, { name: 'Alex' }, { name: 'Mark' }];
+//without distructurization;
+
+// const names = [];
+
+// for (const user of users) {
+//     names.push(user.name)
+// }
+// console.log(names);
+
+//with  distructurization;
+
+// const names = [];
+// for (const { name } of users) {
+//     names.push(name)
+// }
+
+// console.log(names);
+
+
+//******************------SPREAD ----- REST *******/
+
+// function foo(first, second, ...args) { //rest
+//     console.log(first);
+//     console.log(second);
+//     console.log(args);
+// }
+// foo('Alice', 'Kate', 'Emma', 'Mia', 'Lily')
+
+//********************************************** */
+// const user = {
+//     name: 'Test name',
+//     age: 22,
+//     city: 'Lviv',
+//     skills: {
+//         html: true,
+//         css: false, 
+//         js: true
+//     }
+// }
+
+// function foo({ name, age, ...props }) {
+//     console.log(name);
+//     console.log(age);
+//     console.log(props);
+// }
+// foo(user)
+//********************************************* */
+
+// const user = {
+//     id: 1,
+//     username: 'harry_potter',
+//     profile: {
+//         name: 'Harry',
+//         surname: 'Potter',
+//         age: 25
+//     }
+// };
+
+// const { username, profile, profile: { name, surname } } = user;
+
+// console.log(username);
+// console.log(profile);
+// //console.log(name);
+// console.log(surname);
+
+//****************************************** */
+// 1 EXAMPLE ------ rest;
+// function transformUsername({ firstName, lastName, ...props }) {
+//     return {
+//         fullName: `${firstName} ${lastName}`,
+//         ...props
+//     }
+// }
+// 2  EXAMPLE ----- delete;
+// function transformUsername(obj) {
+//     const firstName = obj.firstName;
+//     const lastName = obj.lastName;
+//     const fullName = `${firstName} ${lastName}`
+
+//     delete obj.firstName;
+//     delete obj.lastName;
+
+//     obj.fullName = fullName;
+
+//     return obj;
+
+// }
+
+// console.log(
+//     transformUsername({
+//         id: 1,
+//         firstName: 'Jacob',
+//         lastName: 'Mercer',
+//         email: 'j.mercer@mail.com',
+//         friendCount: 40,
+//     }),
+
+// );
+// console.log(
+//     transformUsername({
+//         id: 2,
+//         firstName: 'Adrian',
+//         lastName: 'Cross',
+//         email: 'a.cross@hotmail.com',
+//         friendCount: 20,
+//     }),
+// );
+
+//**************************************** */
+// function calculateHousePerimeter({ sideA, sideB, sideC, sideD } = {}) {
+//     const perimeter = sideA + sideB + sideC + sideD;
+//     return perimeter;
+// }
+
+// const perimeter = calculateHousePerimeter({
+//     sideA: 10,
+//     sideB: 15,
+//     sideC: 10,
+//     sideD: 15,
+// });
+// console.log(calculateHousePerimeter());
+// console.log(`House perimeter is ${perimeter}`);
+
+
+// /** --- JS doc comment
+//  * Buy product, log message with purchase info
+//  * @param {String} productName 
+//  * @param {Number} productQty 
+//  * @param {Number} price 
+//  */  
+
+// function buyProduct(productName, productQty, price) {
+//     console.log(`You buy ${productQty} ${productName}s, cost per item ${price},
+//     total cost ${productQty * price } ` );
+// }
+// buyProduct("apple", 10, 35);
+
+// ******************** ARROW FUNCTION  *********************
+// const buyProduct = (productName, productQty, price) => {
+//     console.log(`You buy ${productQty} ${productName}s, cost per item ${price},
+//     total cost ${productQty * price } ` );
+// }
+
+// buyProduct("apple", 10, 35);
+
+// const getTotalPrice = (qty, price) => {
+//     return qty * price;
+// };
+
+// console.log(getTotalPrice(3, 150));
+
+//**********NEYAVNE POVERNENNYA (TO SUM, MINUS) -- FOR SHORT OPERATIONS */
+
+// const getTotalPrice = (qty, price) => qty * price;
+// console.log(getTotalPrice(3, 150));
+//******************+ CALLBACK FUNCTION ******************** */
+
+// function logResult(a, b, callback) {
+//     const result = callback(a, b);
+//     console.log(`Result is ${result}`);
+// }
+
+// logResult(4, 5, add)
+// logResult(4, 5, sum)
+
+// function add(a, b) {
+//     return a + b
+// }
+
+// function sum(a, b) {
+//     return a * b
+// }
+
+//******************+ INLINE CALLBACK FUNCTION ******************** */
+
+
+// function logResult(a, b, callback) {
+//     const result = callback(a, b);
+//     console.log(`Result is ${result}`);
+// }
+
+// logResult(4, 5, (a, b) => a + b)
+// logResult(4, 5, (a, b) => a * b)
+
+//******************************************* **********************/
+// function each(arr, action) {
+    // const items = [];
+    // EXAMPLE 1 --- DIFFICULT
+    // arr.forEach((element) => {
+    //     const result = action(element);
+    //     items.push(result);
+    // });
+    // return items;
+
+    // EXAMPLE 2 ----- EASY
+
+    // arr.forEach((element) => items.push(action(element)));
+    // return items;
+
+    // EXAMPLE 3 ----- MAP - right one
+
+//       return arr.map((element) => action(element));
+
+// }
+// console.log(each([64, 49, 36, 25, 16], (value) => value * 2 ));
+
+//******************************************* *********************/
+
+// const products = [
+//     {
+//         ad: 1,
+//         name: "Phone",
+//         price: 1000,
+//         description: "Smartphone"
+
+//     },
+//     {
+//         id: 2,
+//         name: "Notebook",
+//         price: 2500,
+//         description: "Compact computer"
+//     },
+//     {
+//         id: 3,
+//         name: "Computer",
+//         price: 4500,
+//         description: "Powerful computer"
+//     },
+
+// ];
+ 
+// function getProductDetails(id, success, error) {
+//     for (const product of products) {
+//         if (product.id === id) {
+//             success(product);
+//             return;
+//         }
+//     }
+//     error(id)
+// }
+
+// getProductDetails(3, handleSuccess, handleError);
+// getProductDetails(44, handleSuccess, handleError);
+
+//******************************************* *********************/
+// const products = [
+//     { name: "Apple", price: 0.99 },
+//     { name: "Banana", price: 0.49 },
+//     { name: "Orange", price: 0.79 },
+//     { name: "Mango", price: 1.99 },
+// ];
+
+// function calculateAvaragePrice(products, onSuccess, onError) {
+//     if (products.length) {
+//         return onError("No product found");
+//     }
+
+//     let total = 0;
+//     products.forEach(({ price }) => (total += price));
+    
+//     onSuccess(total / products.length)
+// }
+
+// const onSuccess = (averagePrice) => 
+//     console.log("Average price - ", averagePrice.toFixed(2));
+ 
+// const onError = (errorMessage) => 
+//     console.log("Error: ", errorMessage);
+  
+// calculateAvaragePrice(products, onSuccess, onError);
